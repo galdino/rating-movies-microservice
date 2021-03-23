f577e88c68c0c8658f4e1c0a1eaf6ee465b9b8ad
Fernando Galdino <fernando.galdino@meta.com.br>
2021-03-23 08:21:30 -0300

    Refactor for granular fallback

--- movie-catalog-service/src/main/java/br/com/moviecatalogservice/models/UserRating.java
+++ movie-catalog-service/src/main/java/br/com/moviecatalogservice/models/UserRating.java
@@ -4,10 +4,19 @@ import java.util.List;
 
 public class UserRating {
 	
+	private String userId;
+	private List<Rating> userRating;
+	
 	public UserRating() {
 	}
-	
-	List<Rating> userRating;
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
 
 	public List<Rating> getUserRating() {
 		return userRating;
